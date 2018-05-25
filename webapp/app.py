8c78b88ed5d1a19f2b7d33cfeab7856bbc9d9cb1
Prabhath Kotha <prabhath.k3@gmail.com>
2018-05-24 22:38:23 -0400

    frontend improvements, no post functionality

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, request
 import os.path
 app = Flask("DynamicLocus")
 
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, 'content.html')
 
-@app.route('/')
-def static_page():
-    return render_template(filename)
+@app.route('/', methods=["GET"])
+def home():
+    return """
+        <!DOCTYPE html>
+        <html lang="en">
+
+	        <head>
+		        <meta charset="utf-8"/>
+		        <title>Sample</title>
+	        </head>
+	
+            <body>
+                <h1>
+                    test header 1 updated
+                </h1>
+                html content rendered!
+            </body>
+           
+        </html>"""
+
+@app.route('/', methods=['GET', 'POST'])
+def content():
+    error = None
+    if request.method == 'POST':
+        
